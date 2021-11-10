5563876928f393695227df31dd6da28cb67741fc
donaldterance <donaldterance@gmail.com>
2021-11-10 17:04:32 -0600

    added changes for production

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import users from './routes/users.js';
 import auth from './routes/auth.js';
 import contact from './routes/contacts.js';
 import connectdb from './config/db.js';
+import path from 'path';
 const app = express();
 
 //Connect Database
@@ -18,6 +19,15 @@ const PORT = process.env.PORT || 5001;
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/contact', contact);
+
+//serve static assets in prod
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
